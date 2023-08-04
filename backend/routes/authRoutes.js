01e2e7fcfc1d41d1c4b2d7e15c7e28e224a9141a
lakee mangal <lakee.12002794@lpu.in>
2023-08-04 14:46:26 +0530

    minor change

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const authController = require('../controllers/authController');
+const authController = require('../controllers/authControllers');
 
 const router = express.Router();
 
